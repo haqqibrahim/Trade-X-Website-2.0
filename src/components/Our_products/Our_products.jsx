94bf2f9e790d85e2e035427d1ab044335c71b615
ctohaqq <enessyibrahim@gmail.com>
2022-04-21 18:49:54 +0100

    Video update

--- src/components/Our_products/Our_products.jsx
+++ src/components/Our_products/Our_products.jsx
@@ -1,9 +1,6 @@
 // OUR PRODUCT COMPONENT
 import Header from './Header_Our_products';
-<<<<<<< HEAD
 import Miniheader from "../Home/Miniheader";
-=======
->>>>>>> ce3bb2c805d103dce82f8b1aac52014765c4b850
 
 import React from 'react';
 
@@ -11,10 +8,7 @@ function Our_products() {
   return (
     <>
       <Header />
-<<<<<<< HEAD
 	  <Miniheader />
-=======
->>>>>>> ce3bb2c805d103dce82f8b1aac52014765c4b850
     </>
   );
 }
